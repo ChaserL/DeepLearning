333e684f4a3c81fc6675e57543079774fea88fb7
Sam <406284813@qq.com>
2018-10-31 09:28:07 +0800

    try

--- version1/BP.py
+++ version1/BP.py
@@ -5,3 +5,13 @@ Created on Tue Oct  19 14:36:17 2018
 @author: Sam
 """
 
+
+def cc(startnumb,endnumb):
+    initnumb = 0
+    while(startnumb<=endnumb):
+        startnumb = startnumb * 1.1
+        initnumb = initnumb + 1
+    print(initnumb)
+    
+    
+cc(4,8)
